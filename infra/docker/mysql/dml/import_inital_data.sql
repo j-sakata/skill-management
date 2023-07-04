129dca1863896c03de65c58915ff4def54680e1a
Joji_Sakata <helloworld01joji25@gmail.com>
2023-07-04 12:19:52 +0900

    設定変更実行追加

--- infra/docker/mysql/dml/import_inital_data.sql
+++ infra/docker/mysql/dml/import_inital_data.sql
@@ -1,4 +1,5 @@
 -- local_infile の設定に応じて、サーバーはローカルデータロードをリクエストするクライアントによるローカルデータロードを拒否または許可する。
+SET PERSIST local_infile= 1;
 SHOW VARIABLES LIKE 'local_infile';
 
 load data local infile '/tmp/dml/skill_list.csv' into table skill_masters fields terminated by ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES;
