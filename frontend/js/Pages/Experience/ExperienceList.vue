8c897c7fae76f7886bb2e5122631130729d252a4
Joji_Sakata <helloworld01joji25@gmail.com>
2024-07-02 23:20:31 +0900

    polish

--- frontend/js/Pages/Experience/ExperienceList.vue
+++ frontend/js/Pages/Experience/ExperienceList.vue
@@ -29,173 +29,155 @@
       ></technical-skill-list>
     </v-dialog>
     <v-row no-gutters>
-      <v-col cols="6">
-        <v-row no-gutters class="pa-4 pb-0">
-          <v-col cols="8">
-            <v-text-field
-              v-model="keyword"
-              label="キーワード"
-              outlined
-              persistent-placeholder
-              clearable
-              dense
-              hide-details="auto"
-            ></v-text-field>
-          </v-col>
-          <v-col cols="auto" class="ml-2">
-            <v-btn dense @click="search">検索</v-btn>
-          </v-col>
-          <v-spacer></v-spacer>
-          <v-col cols="auto">
-            mode： {{ mode.valueName }}
-          </v-col>
-        </v-row>
+      <v-col
+        cols="auto"
+      >
+        <v-tabs
+          vertical
+          color="indigo lighten-1"
+        >
+          <v-tab v-for="(n, i) in mode.key" v-bind:key="n" @click=changeMode(i)>
+            {{ mode.value[i] }}
+          </v-tab>
+        </v-tabs>
       </v-col>
-      <v-col cols="6">
-        <v-row no-gutters class="pa-4 pb-0">
-          <v-spacer></v-spacer>
-          <v-col cols="2">
-            <v-btn dense block @click="modal.register = true">新規作成</v-btn>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col cols="6" class="pa-5">
-        <!-- 職務経歴モード -->
-        <v-row no-gutters v-if="mode.keyName == 'jobCareer'">
-          <v-col class="pa-0 ma-0">
-            <v-data-table
-              :headers="experienceHeaders"
-              :items="experienceContentItems"
-              class="elevation-1"
-              @click:row="select"
-              dense
-              height=230
-            >
-            </v-data-table>
-          </v-col>
-        </v-row>
-        <!-- 職務要約モード -->
-        <v-row no-gutters v-if="mode.keyName == 'jodSummary'">
-          <v-col class="pa-0 ma-0">
-            <v-data-table
-              :headers="experienceSummaryHeaders"
-              :items="cloneExperienceSummaries"
-              class="elevation-1"
-              @click:row="select"
-              dense
-              height=230
-            >
-              <template v-slot:items="props">
-                <td>{{ props.item.title }}</td>
-                <td>{{ props.item.status | statusType }}</td>
-              </template>
-            </v-data-table>
-          </v-col>
-        </v-row>
-        <!-- 活かせる経験・知識モード -->
-        <v-row no-gutters v-if="mode.keyName == 'jobKnowledge'">活かせる経験・知識</v-row>
-        <!-- 自己PRモード -->
-        <v-row no-gutters v-if="mode.keyName == 'character'">自己PR</v-row>
+      <v-col>
         <v-row no-gutters>
-          <v-col cols="8" class="ma-0 pa-0">
-            <v-card outlined class="mt-6" height="230">
-              <v-toolbar
-                flat
-                color="indigo lighten-1"
-                dark
-                dense
-                height="35"
-              >
-                <v-toolbar-title>テクニカルスキル</v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-btn
-                  color="indigo darken-2"
-                  dark
-                  x-small
-                  @click="modal.skillList = true"
-                >
-                  view more...
-                </v-btn>
-              </v-toolbar>
-              <v-tabs vertical color="indigo lighten-1" class="overflow-y-auto" height="193">
-                <v-tab>
-                  言語/API
-                </v-tab>
-                <v-tab>
-                  フレームワーク、DB等
-                </v-tab>
-                <v-tab>
-                  OS、クラウド等
-                </v-tab>
-                <v-tab>
-                  ルータ、NW技術等
-                </v-tab>
-                <v-tab>
-                  プロジェクト支援
-                </v-tab>
-                <v-tab-item v-for="n in 5" :key="n">
-                  <v-card flat>
-                    <v-card-text>
-                      <v-data-table
-                        :headers="technicalHeaders"
-                        :items="technicalSkillItems(n)"
-                        class="elevation-1"
-                        dense
-                        disable-sort
-                        :items-per-page="4"
-                        hide-default-footer
-                      >
-                        <template v-slot:items="props">
-                          <td>{{ props.item.name }}</td>
-                        </template>
-                      </v-data-table>
-                    </v-card-text>
-                  </v-card>
-                </v-tab-item>
-              </v-tabs>
-            </v-card>
+          <v-col cols="6">
+            <v-row no-gutters class="pa-4 pb-0">
+              <v-col cols="8">
+                <v-text-field
+                  v-model="keyword"
+                  label="キーワード"
+                  outlined
+                  persistent-placeholder
+                  clearable
+                  dense
+                  hide-details="auto"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="auto" class="ml-2">
+                <v-btn dense @click="search">検索</v-btn>
+              </v-col>
+            </v-row>
           </v-col>
-          <v-col cols="4" class="ma-0 pa-0">
-            <v-card outlined class="mt-6" height="230">
-              <v-toolbar
-                flat
-                color="indigo lighten-1"
-                dark
-                dense
-                height="35"
-              >
-                <v-toolbar-title>モード切替</v-toolbar-title>
-              </v-toolbar>
-              <v-card-text>
-                <v-row dense>
-                  <v-col>
-                    <v-btn dense height="45" width="100" block @click=changeMode(0)>{{ mode.value[0] }}</v-btn>
-                  </v-col>
-                </v-row>
-                <v-row dense class="my-1">
-                  <v-col>
-                    <v-btn dense height="45" width="100" block @click=changeMode(1)>{{ mode.value[1] }}</v-btn>
-                  </v-col>
-                </v-row>
-                <v-row dense>
-                  <v-col>
-                    <v-btn dense height="45" width="100" block @click=changeMode(2)>{{ mode.value[2] }}</v-btn>
-                  </v-col>
-                </v-row>
-              </v-card-text>
-            </v-card>
+          <v-col cols="6">
+            <v-row no-gutters class="pa-4 pb-0">
+              <v-spacer></v-spacer>
+              <v-col cols="2">
+                <v-btn dense block @click="modal.register = true">新規作成</v-btn>
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
-      </v-col>
-      <v-col cols="6">
-        <experience-detail
-          :selected="selected"
-          :skillMaster="skill_master"
-          :detailType="mode.keyName"
-          @edit="edit"
-        ></experience-detail>
+        <v-row no-gutters>
+          <v-col cols="6" class="pa-5">
+            <!-- 職務経歴モード -->
+            <v-row no-gutters v-if="mode.keyName == 'jobCareer'">
+              <v-col class="pa-0 ma-0">
+                <v-data-table
+                  :headers="experienceHeaders"
+                  :items="experienceContentItems"
+                  class="elevation-1"
+                  @click:row="select"
+                  dense
+                  height=230
+                >
+                </v-data-table>
+              </v-col>
+            </v-row>
+            <!-- 職務要約モード -->
+            <v-row no-gutters v-if="mode.keyName == 'jodSummary'">
+              <v-col class="pa-0 ma-0">
+                <v-data-table
+                  :headers="experienceSummaryHeaders"
+                  :items="cloneExperienceSummaries"
+                  class="elevation-1"
+                  @click:row="select"
+                  dense
+                  height=230
+                >
+                  <template v-slot:items="props">
+                    <td>{{ props.item.title }}</td>
+                    <td>{{ props.item.status | statusType }}</td>
+                  </template>
+                </v-data-table>
+              </v-col>
+            </v-row>
+            <!-- 活かせる経験・知識モード -->
+            <v-row no-gutters v-if="mode.keyName == 'jobKnowledge'">活かせる経験・知識</v-row>
+            <!-- 自己PRモード -->
+            <v-row no-gutters v-if="mode.keyName == 'character'">自己PR</v-row>
+            <v-row no-gutters>
+              <v-col cols="12" class="ma-0 pa-0">
+                <v-card outlined class="mt-6" height="230">
+                  <v-toolbar
+                    flat
+                    color="indigo lighten-1"
+                    dark
+                    dense
+                    height="35"
+                  >
+                    <v-toolbar-title>テクニカルスキル</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      color="indigo darken-2"
+                      dark
+                      x-small
+                      @click="modal.skillList = true"
+                    >
+                      view more...
+                    </v-btn>
+                  </v-toolbar>
+                  <v-tabs vertical color="indigo lighten-1" class="overflow-y-auto" height="193">
+                    <v-tab>
+                      言語/API
+                    </v-tab>
+                    <v-tab>
+                      フレームワーク、DB等
+                    </v-tab>
+                    <v-tab>
+                      OS、クラウド等
+                    </v-tab>
+                    <v-tab>
+                      ルータ、NW技術等
+                    </v-tab>
+                    <v-tab>
+                      プロジェクト支援
+                    </v-tab>
+                    <v-tab-item v-for="n in 5" :key="n">
+                      <v-card flat>
+                        <v-card-text>
+                          <v-data-table
+                            :headers="technicalHeaders"
+                            :items="technicalSkillItems(n)"
+                            class="elevation-1"
+                            dense
+                            disable-sort
+                            :items-per-page="4"
+                            hide-default-footer
+                          >
+                            <template v-slot:items="props">
+                              <td>{{ props.item.name }}</td>
+                            </template>
+                          </v-data-table>
+                        </v-card-text>
+                      </v-card>
+                    </v-tab-item>
+                  </v-tabs>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="6">
+            <experience-detail
+              :selected="selected"
+              :skillMaster="skill_master"
+              :detailType="mode.keyName"
+              @edit="edit"
+            ></experience-detail>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
@@ -234,8 +216,8 @@ export default {
       mode: {
         keyName: null,
         valueName: null,
-        key: [],
-        value: []
+        key: ["jobCareer", "jodSummary", "jobKnowledge", "character"],
+        value: ["職務経歴", "職務要約", "活かせる経験・知識", "自己PR"]
       },
       experienceHeaders: [
         {text: "プロジェクト名", value: "project_name"},
@@ -255,7 +237,6 @@ export default {
   },
   mounted() {
     this.init();
-    this.setModeName()
   },
   computed: {
     experienceContentItems() {
@@ -310,7 +291,6 @@ export default {
     changeMode(number) {
       this.mode.keyName = this.mode.key[number]
       this.mode.valueName = this.mode.value[number]
-      this.setModeName()
       if (this.mode.keyName == 'jobCareer') {
         this.selected = this.cloneExperiences[0];
       }else if (this.mode.keyName == 'jodSummary') {
